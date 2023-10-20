d95e2045d0ac02e230f482b6a3643d0dbb9b26dd
William Mills <wmpmills@gmail.com>
2023-10-20 14:09:25 +0100

    Update kiosk-controls.js

--- kiosk-controls.js
+++ kiosk-controls.js
@@ -1,72 +1,60 @@
 /********************************************************
-Copyright (c) 2022 Cisco and/or its affiliates.
-This software is licensed to you under the terms of the Cisco Sample
-Code License, Version 1.1 (the "License"). You may obtain a copy of the
-License at
-               https://developer.cisco.com/docs/licenses
-All use of the material herein must be in accordance with the terms of
-the License. All rights not expressly granted by the License are
-reserved. Unless required by applicable law or agreed to separately in
-writing, software distributed under the License is distributed on an "AS
-IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
-or implied.
-*********************************************************
  * 
  * Macro Author:      	William Mills
  *                    	Technical Solutions Specialist 
  *                    	wimills@cisco.com
  *                    	Cisco Systems
  * 
- * Version: 1-0-0
- * Released: 04/28/22
+ * Version: 2-0-0
+ * Released: 10/20/23
  * 
  * This macro gives you the ability to enable kiosk mode from the touch inteface
  * of your your Webex Devices. If you have a paired Touch 10 or Webex Navigator
- * you can even exit kiosk mode with the macro. It also lets your select between
- * preset Kiosk URLs fromt he macros UI panel.
+ * you can even exit kiosk mode with the macro. It also lets you select between
+ * a preconfigured list of Kiosk URLs from the macros UI panel.
  * 
- * Lastly, the macro also allows you to toggle kiosk mode by sending the special
- * keywords ExistKiosk or EnterKiosk to the device via the Message Send xAPI 
- * command. This can be done from your Web App or its backend.
+ * In the case where you don't have a paired controller to exit Kiosk Mode
+ * you can leverage this example Kiosk Web App which can turn Kiosk Mode off
+ * from the Web App itself: https://github.com/wxsd-sales/exit-kiosk-web-app
+ * 
+ * Full Readme, source code and license details for this macro are available 
+ * on Github: https://github.com/wxsd-sales/auto-share-macro
  * 
  ********************************************************/
 
 import xapi from 'xapi';
+
+xapi.Status.Diagnostics.Message.get()
+.then(result=>console.log(JSON.stringify(result)))
 
 
 /*********************************************************
  * Configure the settings below
 **********************************************************/
 
-
-// Set this to be true if you would like to allow exiting kiosk mode
-// by pressing the Do Not Disturb button in the hidden menu
-const ALLOW_EXIT = true;
-
-// These are recommend settings to disable while in kiosk mode
-// they harden the device and limit settings access
-const DISABLE_SETTINGS = false;
-const DISABLE_ASSISTANT = false;
-const DISABLE_ULTRASOUND = false;
-
-// Specify the button/panel name
-const BUTTON_NAME = 'Kiosk Control';
-
-// Specify the URLs
-// The Text parameter will be shown on the macros UI Panel
-// The URL will be set as the Kiosk URL when selected on the panel toggles
-// Ensure the URLs contain the full address eg (http://www.google.com)
-// Also ensure the same URL isn't used twice
-const KIOSK_URLS = [
-  {
-    "Text" : 'Example Site',
-    "URL" : 'https://www.example.com'
+const config = {
+  pin: '1234',
+  button: {
+    name: 'Kiosk Controls'
   },
-  {
-    "Text" : 'Example Site 2',
-    "URL" : 'https://www.example2.com'
-  }
-];
+  kioskUrls: [
+    {
+      text: 'Exit Kiosk Example',
+      url: 'https://wxsd-sales.github.io/exit-kiosk-web-app/exitKioskWebapp.html?username=kioskwebapp&password=examplePassword'
+    },
+    {
+      text: 'Webex Bank Example',
+      url: 'https://cisco-ce.github.io/roomos-samples/kiosk-example/'
+    }
+  ],
+  allowExit: true,
+  disableSettings: false,
+  disableAssistant: true,
+  disableUltrasound: true,
+  autoCleanup: true,
+  autoEnableKiosk: true,
+  panelId: 'kioskcontrols'
+}
 
 
 
@@ -76,13 +64,14 @@ const KIOSK_URLS = [
 
 
 // This is our main function which initializes everything
-async function main(){
+async function main() {
 
   // Create our Button and UI Panel
-  createPanel();
+  await createPanel();
+  await createButtons();
 
   // Update the UI based off system state
-  await syncUI();
+  syncUI();
 
   // Enable WebEngine and WebGL
   xapi.Config.WebEngine.Mode.set('On');
@@ -90,15 +79,25 @@ async function main(){
 
 
   // Disable/enable recommend features
-  xapi.Config.UserInterface.SettingsMenu.Mode.set(DISABLE_SETTINGS ? 'Locked' : 'Unlocked');
-  xapi.Config.UserInterface.Assistant.Mode.set(DISABLE_ASSISTANT ? 'Off' : 'On');
-  xapi.Config.Audio.Ultrasound.MaxVolume.set(DISABLE_ULTRASOUND ? 0 : 70);
+  xapi.Config.UserInterface.SettingsMenu.Mode.set(config.disableSettings ? 'Locked' : 'Unlocked');
+  xapi.Config.UserInterface.Assistant.Mode.set(config.disableAssistant ? 'Off' : 'On');
+  xapi.Config.Audio.Ultrasound.MaxVolume.set(config.disableUltrasound ? 0 : 70);
 
   // Listen for all toggle events
-  xapi.Event.UserInterface.Extensions.Widget.Action.on(widgetEvent);
+  xapi.Event.UserInterface.Extensions.Widget.Action.on(processWidgets);
+  xapi.Event.UserInterface.Extensions.Panel.Clicked.on(event => {
+    if (!event.PanelId.startsWith(config.panelId)) return;
+    kioskEnabled()
+      .then(enabled => {
+        if (enabled) {
+          askForPIN();
+        } else {
+          xapi.Command.UserInterface.Extensions.Panel.Open({ PanelId: config.panelId });
+        }
+      })
+  });
 
-  // Listen for Message Send events, used to exist Kiosk Mode via Cloud xAPI
-  xapi.Event.Message.Send.on(monitorMessage);
+  xapi.Event.UserInterface.Message.TextInput.Response.on(processTextResponse)
 
   // Monitor Kiosk settings change to keep the UI updated
   xapi.Config.UserInterface.Kiosk.Mode.on(syncUI);
@@ -107,10 +106,38 @@ async function main(){
   // Monitor the response to the warning prompt when no URL is set
   xapi.Event.UserInterface.Message.Prompt.Response.on(warningResponse);
 
-  // Monitor for changes to the SpeakerTrack state so we can exit Kiosk Mode
-  if(ALLOW_EXIT) {
-    xapi.Status.Cameras.SpeakerTrack.Status.on(speakerTrack);
+  xapi.Event.UserInterface.Extensions.Event.PageOpened
+    .on(value => console.log('Page Opened', value));
+
+  xapi.Event.UserInterface.Extensions.Event.PageClosed
+    .on(value => console.log('Page Closed', value));
+
+  if (config.autoCleanup) {
+
+    xapi.Status.Standby.State.on(state => {
+      console.log('Standby State Changed to: ', state)
+
+      if (state != 'Standby') return;
+
+      console.log('Performing Room Clean Up')
+      xapi.Command.RoomCleanup.Run({ ContentType: ['TemporaryAccounts', 'Whiteboards', 'WebData'] });
+
+      if (config.autoEnableKiosk) {
+
+        xapi.Config.UserInterface.Kiosk.Mode.set('On');
+      }
+    });
   }
+
+  xapi.Event.RoomCleanup.Complete.on(value => {
+    console.log('Cleanup result', value)
+  });
+
+  xapi.Event.Standby.SecondsToStandby
+    .on(value => console.log('Seconds to Standby:',value));
+
+    xapi.Event.Standby.Reset
+    .on(value => console.log('Standby Reset Event',value));
 
 }
 
@@ -122,25 +149,13 @@ main();
  * Below are the function which this macro uses
 **********************************************************/
 
-// This function is called when speakerTrack is enable/disable
-// using this while in kiosk mode, we can provide a way to 
-// easily have our macro disable kiosk mode and return to normal
-async function speakerTrack(event) {
 
-  const state = getKioskMode();
-
-  if(state == 'On' && event == 'Inactive') {
-    console.log('Disabling Kiosk Mode')
-    setKioskMode('Off');
-  }
-
-}
-
-async function getKioskMode() {
-  return await xapi.Config.UserInterface.Kiosk.Mode.get();
+function kioskEnabled() {
+  return xapi.Config.UserInterface.Kiosk.Mode.get().then(result => result === 'On')
 }
 
 async function setKioskMode(mode) {
+  console.log('Setting Kiosk Mode: ', mode);
   xapi.Config.UserInterface.Kiosk.Mode.set(mode);
 }
 
@@ -152,48 +167,24 @@ async function setKioskURL(url) {
   xapi.Config.UserInterface.Kiosk.URL.set(url);
 }
 
-// This function is called when a xAPI Send Message is called
-// it will exit kiosk mode when it heards the keyword ExistKiosk
-function monitorMessage(event) {
-
-  if(event.Text == 'ExitKiosk'){
-    console.log('Closing Kiosk');
-    toggleKiosk(false);
-  }
-
-}
 
 // This function will toggle kiosk mode
 // It will also check a Kiosk URL is set before enabling and
 // warn the user of this case
-async function toggleKiosk(state){
-
-  if(!state) {
-    console.log('Disabling Kiosk Mode');
-    setKioskMode('Off');
+async function toggleKiosk() {
+  const state = await xapi.Config.UserInterface.Kiosk.Mode.get()
+  if (state == 'On') {
+    setKioskMode('Off')
   } else {
-    const currentURL = await getKioskUrl();
-
-    if(currentURL == ''){
-      console.log('No Kiosk URL set, warning user');
-      warnUser();
-    } else {
-      console.log('Enabling Kiosk Mode');
-      setKioskMode('On');
-    }
-
+    setKioskMode('On')
   }
-
-  syncUI();
-
 }
 
 // This function is ued to diaplay a warning prompt when no Kiosk URL is set
 function warnUser() {
-
   xapi.Command.UserInterface.Message.Prompt.Display(
     {
-      Title: BUTTON_NAME,
+      Title: config.button.name,
       Text: 'Warning: No Kiosk URL configured. The device will show as Out of Service. Are you sure you want to enable Kiosk Mode?',
       FeedbackId: 'kiosk_warning',
       "Option.1": "Yes",
@@ -203,11 +194,7 @@ function warnUser() {
 
 // Here we handle the user response to the warning message
 function warningResponse(response) {
-
-  if(response.FeedbackId != 'kiosk_warning') {
-    return;
-  }
-  
+  if (response.FeedbackId != 'kiosk_warning') return;
   switch (response.OptionId) {
     case '1':
       console.log('Warning accepted, enabling Kiosk Mode');
@@ -218,128 +205,171 @@ function warningResponse(response) {
       syncUI();
       break;
   }
-
 }
 
+function processTextResponse(event) {
+  if (!event.FeedbackId.startsWith(config.panelId)) return;
+  const responseType = event.FeedbackId.split('-').pop();
+  switch (responseType) {
+    case 'pin':
+      if (event.Text == config.pin) {
+        console.log('PIN valid - opening panel');
+        xapi.Command.UserInterface.Extensions.Panel.Open({ PanelId: config.panelId });
+        return;
+      } else {
+        console.log('Invalid PIN - notify user');
+        xapi.Command.UserInterface.Message.Alert.Display(
+          { Duration: 5, Text: 'The PIN entered was invalid, please try again', Title: 'Invalid PIN' });
+      }
+      break;
+  }
+}
 
 // This fucntion will set the new Kiosk URL
-async function setUrl(widget) {
-
-  // Identify which Kiosk Toggle widget was selected
-  const urlNumber = widget.charAt(widget.length - 1);
-
-  // Find the appropriate URL from our URL array
-  const url = KIOSK_URLS[urlNumber].URL;
-
-  console.log('Kiosk URL set to: ' + url);
-
+async function setUrl(selection) {
+  const url = config.kioskUrls[selection].url;
+  console.log('Setting Kiosk URL set to: ' + url);
   setKioskURL(url);
-
-  syncUI();
-  
+  //syncUI();
 }
 
+function compareURLs(a, b) {
+  a = decodeURIComponent(a).trim();
+  b = decodeURIComponent(b).trim();
+  return a.includes(b)
+}
 
 // This function will get the devices current state and update
 // the UI to display the correct state
 async function syncUI() {
-
-  console.log('Syncing UI')
-
-  // Check if there is already a URL set
+  console.log('Syncing UI');
+  await createPanel();
   const currentURL = await getKioskUrl();
+  const selected = config.kioskUrls.findIndex(site => compareURLs(site.url, currentURL));
+  if (selected == -1) {
+    xapi.Command.UserInterface.Extensions.Widget.UnSetValue({ WidgetId: config.panelId + '-siteGroup' });
+  } else {
+    xapi.Command.UserInterface.Extensions.Widget.SetValue(
+      { Value: selected + 1, WidgetId: config.panelId + '-siteGroup' });
+  }
+  
+}
 
-  // Update the site selection panel
-  KIOSK_URLS.forEach((site, i) => {
-
-    const match = currentURL == site.URL;
-
-    xapi.Command.UserInterface.Extensions.Widget.SetValue({
-        WidgetId: 'kiosk_site_'+i,
-        Value: match ? 'on' : 'off',
-      });
-
+function askForPIN() {
+  console.log('Asking for PIN input')
+  xapi.Command.UserInterface.Message.TextInput.Display({
+    FeedbackId: config.panelId + '-pin',
+    InputType: 'PIN',
+    Placeholder: 'Please Enter PIN',
+    SubmitText: 'Submit',
+    Text: 'Please Enter PIN',
+    Title: config.button.name
   });
-
-  // Update the Kiosk toggle panel
-  const kioskState = await getKioskMode();
-
-  xapi.Command.UserInterface.Extensions.Widget.SetValue({
-    WidgetId: 'kiosk_toggle',
-    Value: kioskState,
-  });
-
 }
 
 
 // This function monitors the UI events
-function widgetEvent(event){
-
-  if (event.WidgetId == 'kiosk_toggle') {
-    toggleKiosk(event.Value === 'on');
-  } else if (event.WidgetId.startsWith('kiosk_site_')){
-    setUrl(event.WidgetId);
+function processWidgets(event) {
+  if (!event.WidgetId.startsWith(config.panelId)) return;
+  console.log(event)
+  const widget = event.WidgetId.split('-').pop();
+  switch (widget) {
+    case 'siteGroup':
+      if (event.Type != 'pressed') return
+      setUrl(event.Value - 1);
+      break;
+    case 'toggle':
+      if (event.Type != 'clicked') return
+      toggleKiosk();
+      break;
   }
-
 }
 
 
-// Here we create the Button and Panel for the Macro UI
+// Here we create main hidden panel
 async function createPanel() {
-
-  let sites = '';
-
-  // Create our rows of sites based off the provided array
-  KIOSK_URLS.forEach( (site, i) => {
-
-    const row = `
-      <Row>
-        <Name>${site.Text}</Name>
-        <Options>size=1</Options>
-        <Widget>
-          <WidgetId>kiosk_site_${i}</WidgetId>
-          <Type>ToggleButton</Type>
-          <Options>size=1</Options>
-        </Widget>
-      </Row>`;
-
-    sites = sites.concat(row);
-
+  const button = config.button;
+  const sites = config.kioskUrls;
+  const panelId = config.panelId;
+  let siteGroup = `<Widget>
+                  <WidgetId>${panelId}-siteGroup</WidgetId>
+                  <Type>GroupButton</Type>
+                  <Options>size=4;columns=1</Options>
+                  <ValueSpace>`;
+  sites.forEach((site, i) => {
+    siteGroup = siteGroup.concat(`<Value><Key>${i + 1}</Key><Name>${site.text}</Name></Value>`);
   })
+  siteGroup = siteGroup.concat('</ValueSpace></Widget>');
+  const orderNum = await panelOrder(panelId)
+  const kioskModeEanabled = await kioskEnabled();
 
-  const panel = `
-  <Extensions>
-    <Version>1.8</Version>
-    <Panel>
-      <Order>1</Order>
-      <Type>Statusbar</Type>
-      <Icon>Sliders</Icon>
-      <Color>#CF7900</Color>
-      <Name>${BUTTON_NAME}</Name>
-      <ActivityType>Custom</ActivityType>
-      <Page>
-        <Name>Site Select</Name>
-        ${sites}
-      </Page>
-      <Page>
-        <Name>Kiosk Mode</Name>
-        <Row>
-          <Name>Kiosk Mode</Name>
-          <Widget>
-            <WidgetId>kiosk_toggle</WidgetId>
-            <Type>ToggleButton</Type>
-            <Options>size=1</Options>
-          </Widget>
-        </Row>  
-        <Options/>
-      </Page>
-    </Panel>
-  </Extensions>`;
+  console.log('Kiosk Mode Enabled: ', kioskModeEanabled);
 
+  const order = (orderNum != -1) ? `<Order>${orderNum}</Order>` : '';
+  const panel = `<Extensions>
+                  <Panel>
+                    <Location>Hidden</Location>
+                    <Icon>Sliders</Icon>
+                    <Color>#CF7900</Color>
+                    <Name>${button.name}</Name>
+                    ${order}
+                    <ActivityType>Custom</ActivityType>
+                    <Page>
+                      <Name>Site Select</Name>
+                      <Row>
+                      ${siteGroup}
+                      </Row> 
+                      <Options>hideRowNames=1</Options>
+                    </Page>
+                    <Page>
+                      <Name>Kiosk Mode</Name>
+                      <Row>
+                        <Name>Kiosk Mode</Name>
+                        <Widget>
+                          <WidgetId>${panelId}-toggle</WidgetId>
+                          <Name>${kioskModeEanabled ? 'Disable Kiosk Mode' : 'Enable Kiosk Mode'}</Name>
+                          <Type>Button</Type>
+                          <Options>size=2</Options>
+                      </Widget>
+                      </Row>  
+                      <Options>hideRowNames=1</Options>
+                    </Page>
+                  </Panel>
+                </Extensions>`;
+  await xapi.Command.UserInterface.Extensions.Panel.Save({ PanelId: panelId }, panel)
+  .catch(error=>console.log(`Unable to save panel [${panelId}] - `, e.Message))
+}
 
-  xapi.Command.UserInterface.Extensions.Panel.Save(
-    { PanelId: 'kiosk_panel' }, 
-    panel
-  )
-  
+// Here we create main initial buttons which can open the hidden panel
+async function createButtons() {
+  const button = config.button;
+  const panelId = config.panelId;
+  const locations = ['HomeScreen', 'ControlPanel'];
+  await locations.forEach(async location => {
+    const orderNum = await panelOrder(panelId + location)
+    const order = (orderNum != -1) ? `<Order>${orderNum}</Order>` : '';
+    const panel = `<Extensions>
+                  <Panel>
+                    <Location>${location}</Location>
+                    <Icon>Sliders</Icon>
+                    <Color>#0000ff</Color>
+                    <Name>${button.name}</Name>
+                    ${order}
+                    <ActivityType>Custom</ActivityType>
+                  </Panel>
+                </Extensions>`;
+    await xapi.Command.UserInterface.Extensions.Panel.Save({ PanelId: panelId + location }, panel)
+    .catch(error=>console.log(`Unable to save panel [${panelId}] - `, e.Message))
+  })
+}
+
+// This function finds the place order of the panel if it was saved previously
+async function panelOrder(panelId) {
+  const list = await xapi.Command.UserInterface.Extensions.List({ ActivityType: 'Custom' });
+  if (!list.Extensions.hasOwnProperty('Panel')) return -1
+  if (list.Extensions.Panel.length == 0) return -1;
+  for (let i = 0; i < list.Extensions.Panel.length; i++) {
+    if (list.Extensions.Panel[i].PanelId == panelId) return list.Extensions.Panel[i].Order;
+  }
+  return -1;
 }
